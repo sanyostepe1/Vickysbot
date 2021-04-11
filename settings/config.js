a4abbe2d15abf87a151cf3a8ac8e845daeb1d274
victor grança <vitinho22maluco.2017@gmail.com>
2021-04-11 16:38:35 -0300

    First commit

---
+++ settings/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'Vicky_BOT',
+        ownerName: 'victor frança',
+        youtube: 'https://youtube.com/channel/UCpB3qh2Sp3K23s9G-g',
+        instagram: 'https://instagram.com/animexx_png?igshid=e7zziiwnd9y',
+}
